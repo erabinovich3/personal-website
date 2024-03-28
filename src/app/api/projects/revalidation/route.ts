748e7700ef8ce98d3264450c0ddaa6054a227d20
Erica Rabinovich <ericar13@live.com>
2024-03-28 11:03:00 -0400

    implementing env variable validation

--- src/app/api/projects/revalidation/route.ts
+++ src/app/api/projects/revalidation/route.ts
@@ -1,16 +1,12 @@
 import { NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
-import { strict as assert } from "assert";
-
-const CONTENTFUL_REVALIDATE_SECRET = process.env.CONTENTFUL_REVALIDATE_SECRET;
-
-assert(CONTENTFUL_REVALIDATE_SECRET);
+import { env } from "@/lib/env";
 
 export async function POST(request: Request) {
     const requestHeaders = new Headers(request.headers);
     const secret = requestHeaders.get("x-vercel-reval-key");
 
-    if (secret !== CONTENTFUL_REVALIDATE_SECRET) {
+    if (secret !== env.CONTENTFUL_REVALIDATE_SECRET) {
         return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
     }
 
--- src/lib/contentfulApi.ts
+++ src/lib/contentfulApi.ts
@@ -1,18 +1,12 @@
 import { TypeProjectSkeleton } from "@/@types/contentful";
 import { Project } from "@/model/Project";
 import { parseTypeProjectSkeletonToProject } from "@/utils/parseEntryToProject";
-import { strict as assert } from "assert";
 import { createClient } from "contentful";
-
-const CONTENTFUL_ACCESS_TOKEN = process.env.CONTENTFUL_ACCESS_TOKEN;
-const CONTENTFUL_SPACE_ID = process.env.CONTENTFUL_SPACE_ID;
-
-assert(CONTENTFUL_ACCESS_TOKEN);
-assert(CONTENTFUL_SPACE_ID);
+import { env } from "./env";
 
 export const client = createClient({
-    accessToken: CONTENTFUL_ACCESS_TOKEN,
-    space: CONTENTFUL_SPACE_ID,
+    accessToken: env.CONTENTFUL_ACCESS_TOKEN,
+    space: env.CONTENTFUL_SPACE_ID,
 });
 
 export async function getAllProjects(): Promise<Project[]> {
